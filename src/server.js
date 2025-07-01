99e152de4418e5e4827d8ef7a5e74e978040cf17
Aadi Jain <aadijaintikamgarh@gmail.com>
2025-07-02 02:34:06 +0530

    add custom server and socket helper

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 8000;
+
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    console.log();
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.log(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http:/${hostname}:${port}`);
+    });
+});
